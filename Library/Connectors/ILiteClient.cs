689e374acb2309e5f86b3058ef1ae7fdfe01685f
coquencio <jorge.dm97@gmail.com>
2026-04-12 02:49:03 -0600

    Fixes

--- Library/Connectors/ILiteClient.cs
+++ Library/Connectors/ILiteClient.cs
@@ -1,7 +1,6 @@
 ﻿namespace LiteAgent.Connectors;
 public interface ILiteClient
 {
-    void Setup();
     void SetMaxTokens(int maxTokens);
     void SetTemperature(float temperature);
     Task<string> GetCompletionAsync(List<LiteMessage> history);
